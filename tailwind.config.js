d5665cea85ad5e99b016a0e42231c224f41e7fe0
andrempinto <a104267@alunos.uminho.pt>
2025-11-21 01:49:18 +0000

    s

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Incluir todos os arquivos HTML no diretório raiz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
